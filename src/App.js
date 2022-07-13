6cf6fde63af99a7e5443653d298aae39e89804aa
Natália Diniz <98323720+nataliakdiniz@users.noreply.github.com>
2022-07-13 14:54:54 -0300

    componentes criados

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './components/Header'
 import GlobalStyle from './styles/global'
+import Resume from './components/Resume'
 
 const App = () => {
   return (
     <>
       <Header />
+      <Resume />
       <GlobalStyle />
     </>
   )
