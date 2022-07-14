97ec11403e6b5e269e8fc29885bcdda0b198e889
Natália Diniz <98323720+nataliakdiniz@users.noreply.github.com>
2022-07-14 16:41:14 -0300

    created components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import Header from './components/Header'
+import React, { useEffect, useState } from 'react'
 import GlobalStyle from './styles/global'
+import Header from './components/Header'
 import Resume from './components/Resume'
+import Form from './components/Form'
 
 const App = () => {
   return (
     <>
       <Header />
       <Resume />
+      <Form />
       <GlobalStyle />
     </>
   )
